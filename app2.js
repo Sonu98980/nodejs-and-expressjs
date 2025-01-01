8a1a53737aa4d8dc0368622de8c4e76b92570da5
Sonu98980 <yoursatyabhai2323@gmail.com>
2024-12-31 15:34:19 -1100

    first commit

---
+++ app2.js
@@ -0,0 +1,65 @@
+
+// CREATING SERVER
+
+// const express = require("express");
+// const app = express();
+// // app.listen(8620, () => {      \\ 1
+//     console.log("server listen at port 8620")
+// });
+
+//
+
+// const express = require("express");
+// const app = express();
+// // app.listen(3456, () => {         // 2  PRACTICE
+// //     console.log("server listen at port 3456")
+// // });
+
+
+
+// // // BUILDING ROUTES //API/LOGIN AISA
+
+// // // app.get
+// // // app.post
+// // // app.put              // METHODS
+// // // app.patch
+// // // app.delete
+
+
+
+// app.post("/api/v1/user/home", (req,res) => {
+
+//     const obj = {
+//         name : "sonu",
+//         age : 20,
+//         email : "xyz@gmail.com"
+
+//     };
+  
+//     res.send(obj); 
+// });
+
+// app.get("/api/v1/user/about", (req,res) => {
+//     res.send("<h1> About Page </h1>")
+// })
+// app.get("/api/v1/user/about", (req,res) => {
+//     res.send ( "<h1>about page </h1>")
+// });
+
+// app.get("/api/v1/user/profile", (req,res) => {
+//     res.status(200).json({
+//         success : true,
+//         user : {
+//             username: "donu7889",
+//             email : "hjfdjd"
+//         }
+//     })
+// })
+
+
+
+// app.listen(3786, () => {         
+//     console.log("server listen at port 3786") 
+// });  
+ 
+ 
